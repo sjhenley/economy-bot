aadba019cbea31b9a71f54d807e455e5b9dd06d0
Sam Henley <samueljhenley@gmail.com>
2022-01-01 03:49:24 -0500

    hide aws credentials

--- src/dynamo/dynamo.ts
+++ src/dynamo/dynamo.ts
@@ -4,8 +4,6 @@ const docClient = new AWS.DynamoDB.DocumentClient({
   region: 'us-east-2',
   endpoint: 'https://dynamodb.us-east-2.amazonaws.com',
   apiVersion: 'latest',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 export default docClient;
