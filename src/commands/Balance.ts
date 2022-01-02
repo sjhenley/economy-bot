06aeb32104b5da71459de6d49ffaec829abe1d1d
Sam Henley <samueljhenley@gmail.com>
2022-01-01 21:31:35 -0500

    revert balance reply to followUp

--- src/commands/Balance.ts
+++ src/commands/Balance.ts
@@ -25,7 +25,7 @@ const Balance: Command = {
 
     const balance = await balanceService.getBalance(parseInt(targetUser.id, 10));
 
-    await interaction.reply({
+    await interaction.followUp({
       ephemeral: true,
       content: `Balance for user ${targetUser.username} is ${balance} loungebucks`,
     });
